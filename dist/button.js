ca08c3ea0a664c0801f172daf7187c33a4338707
kaviyarasan <kaviyarasan@dorustree.in>
2022-06-29 10:33:22 +0530

    initial commit

---
+++ dist/button.js
@@ -0,0 +1,9 @@
+import "./button.css";
+
+function Button() {
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("button", {
+    className: "choicesbutton"
+  }, "Button"));
+}
+
+export default Button;
---
+++ src/component/button.js
@@ -0,0 +1,10 @@
+import "./button.css";
+
+function Button(){
+    return(
+        <div>
+<button className="choicesbutton">Button</button>
+        </div>
+    )
+}
+export default Button;

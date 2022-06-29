ae6f9fed939299b1cc8bdaf4b507806412900a10
kaviyarasan <kaviyarasan@dorustree.in>
2022-06-29 14:54:50 +0530

    version update

--- dist/button.js
+++ dist/button.js
@@ -1,10 +1,12 @@
 import "./button.css";
 import React from "react"
 
-function Button() {
+function Button(props) {
   return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("button", {
-    className: "choicesbutton"
-  }, "Button"));
+    onClick: props.onClick,
+    className: "choicesbutton",
+    "data-testid": props.testid
+  }, props.name));
 }
 
 export default Button;
